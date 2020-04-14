3a5844267b1c736a780b0a3e43dd359f15e95e06
Istvan Szabo <iszabo@endava.com>
2020-04-14 20:54:11 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 System.out.println("Hello");
+	 System.out.println("Istvan");
+	    int PrimulNumar = 100;
+        int AlDoileaNumar = 25;
+        int Rezultat = PrimulNumar + AlDoileaNumar;
+            System.out.println(Rezultat);
+            double Impartire = PrimulNumar / AlDoileaNumar;
+                System.out.println("Rezultatul Impartirii este " +  Impartire);
+                int Operatiune = -5 + 8 * 6;
+                System.out.println(Operatiune);
+                    float Operatiune2 = (55+9) % 9;
+                    System.out.println(Operatiune2);
+                        float Operatiune3 = 20 + -3*5 / 8;
+                        System.out.println(Operatiune3);
+                            float Operatiune4 = 5 + 15 / 3 * 2 - 8 % 3;
+                            System.out.println(Operatiune4);
+    }
+}
